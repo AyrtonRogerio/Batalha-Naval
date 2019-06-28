57e658e262ed26fcac64808b203bbcc4b385bf4f
AyrtonRogerio <ayrton.rogerio1@gmail.com>
2019-06-28 13:24:05 -0300

    Tela jogo completa

--- src/br/edu/psd/batalhanaval/view/TelaJogo.java
+++ src/br/edu/psd/batalhanaval/view/TelaJogo.java
@@ -4,23 +4,17 @@
 package br.edu.psd.batalhanaval.view;
 
 import javax.swing.JPanel;
-import javax.swing.GroupLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.GroupLayout.Alignment;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JLabel;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import java.awt.Font;
-import java.awt.Rectangle;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.Dimension;
 import java.awt.Color;
-import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 
 /**
  * @author ayrton
@@ -31,18 +25,32 @@ public class TelaJogo extends JPanel {
 	private JPanel panel, panelMapJogador, panelMapAdversario, panelEmbarcacoes;
 	
 	private JLabel lblSeuJogo, lblJogoAdversario;
+	private JLabel lblTiro1;
+	private JLabel lblTiro2;
+	private JLabel lblTiro3;
+	private JLabel lblCoordX;
+	private JTextField txtFieldX1;
+	private JTextField txtFieldY1;
+	private JLabel lblY;
+	private JTextField txtFieldY2;
+	private JTextField txtFieldX2;
+	private JTextField txtFieldX3;
+	private JTextField txtFieldY3;
+	private JSeparator separator_1;
+	private JSeparator separator;
+	private JSeparator separator_2;
 	
 	/**
 	 * Create the panel.
 	 */
 	public TelaJogo() {
 		setBackground(Color.BLACK);
-		setSize(new Dimension(1024, 800));
+		setSize(new Dimension(1023, 666));
 		setLayout(null);
 		
 		panel = new JPanel();
 		panel.setSize(new Dimension(1024, 800));
-		panel.setBounds(0, 0, 1024, 704);
+		panel.setBounds(0, 0, 1023, 666);
 		panel.setLayout(null);
 		add(panel);
 		criarPanelMapJogador();
@@ -63,15 +71,84 @@ public class TelaJogo extends JPanel {
 		panel.add(lblJogoAdversario);
 		
 		panelEmbarcacoes = new JPanel();
-		panelEmbarcacoes.setBounds(35, 413, 958, 175);
+		FlowLayout flowLayout = (FlowLayout) panelEmbarcacoes.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		panelEmbarcacoes.setBounds(35, 413, 958, 131);
 		panel.add(panelEmbarcacoes);
+		
+		lblTiro1 = new JLabel("Tiro 1:");
+		lblTiro1.setBounds(303, 564, 59, 15);
+		panel.add(lblTiro1);
+		
+		lblTiro2 = new JLabel("Tiro 2:");
+		lblTiro2.setBounds(374, 564, 46, 15);
+		panel.add(lblTiro2);
+		
+		lblTiro3 = new JLabel("Tiro 3:");
+		lblTiro3.setBounds(442, 564, 46, 15);
+		panel.add(lblTiro3);
+		
+		lblCoordX = new JLabel("Coordenada X:");
+		lblCoordX.setBounds(184, 593, 111, 15);
+		panel.add(lblCoordX);
+		
+		txtFieldX1 = new JTextField();
+		txtFieldX1.setBounds(303, 591, 46, 19);
+		panel.add(txtFieldX1);
+		txtFieldX1.setColumns(10);
+		
+		txtFieldY1 = new JTextField();
+		txtFieldY1.setColumns(10);
+		txtFieldY1.setBounds(303, 617, 46, 19);
+		panel.add(txtFieldY1);
+		
+		lblY = new JLabel("Coordenada Y:");
+		lblY.setBounds(184, 619, 111, 15);
+		panel.add(lblY);
+		
+		txtFieldY2 = new JTextField();
+		txtFieldY2.setColumns(10);
+		txtFieldY2.setBounds(374, 617, 46, 19);
+		panel.add(txtFieldY2);
+		
+		txtFieldX2 = new JTextField();
+		txtFieldX2.setColumns(10);
+		txtFieldX2.setBounds(374, 591, 46, 19);
+		panel.add(txtFieldX2);
+		
+		txtFieldX3 = new JTextField();
+		txtFieldX3.setColumns(10);
+		txtFieldX3.setBounds(442, 591, 46, 19);
+		panel.add(txtFieldX3);
+		
+		txtFieldY3 = new JTextField();
+		txtFieldY3.setColumns(10);
+		txtFieldY3.setBounds(442, 617, 46, 19);
+		panel.add(txtFieldY3);
+		
+		JButton btnDisparar = new JButton("Disparar");
+		btnDisparar.setBounds(540, 614, 116, 25);
+		panel.add(btnDisparar);
+		
+		separator_1 = new JSeparator();
+		separator_1.setBounds(35, 556, 958, 8);
+		panel.add(separator_1);
+		
+		separator = new JSeparator();
+		separator.setOrientation(SwingConstants.VERTICAL);
+		separator.setBounds(510, 52, 2, 350);
+		panel.add(separator);
+		
+		separator_2 = new JSeparator();
+		separator_2.setBounds(35, 408, 956, 10);
+		panel.add(separator_2);
 
 	}
 	
 	public void criarPanelMapJogador() {
 		
 		panelMapJogador = new JPanel();
-		panelMapJogador.setBounds(35, 50, 453, 351);
+		panelMapJogador.setBounds(35, 50, 471, 351);
 		panelMapJogador.setLayout(new GridLayout(17, 17));
 		
 		
@@ -83,7 +160,7 @@ public class TelaJogo extends JPanel {
 	public void criarPanelMapAdversario() {
 		
 		panelMapAdversario = new JPanel();
-		panelMapAdversario.setBounds(540, 50, 453, 351);
+		panelMapAdversario.setBounds(522, 50, 471, 351);
 		panelMapAdversario.setLayout(new GridLayout(17, 17));
 		
 		construirMapa(panelMapAdversario, 17, 17);
@@ -106,6 +183,4 @@ public class TelaJogo extends JPanel {
 				
 				panel.add(label);
 			}
-		
-	
 }
